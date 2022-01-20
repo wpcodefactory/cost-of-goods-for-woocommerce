2ad3752f28c444133b27c0d032de1b27a9cae5db
WP <pablo.sg.pacheco@gmail.com>
2022-01-19 22:05:09 -0300

    v2.5.0

--- cost-of-goods-for-woocommerce.php
+++ cost-of-goods-for-woocommerce.php
@@ -3,13 +3,13 @@
 Plugin Name: Cost of Goods for WooCommerce
 Plugin URI: https://wpfactory.com/item/cost-of-goods-for-woocommerce/
 Description: Save product purchase costs (cost of goods) in WooCommerce. Beautifully.
-Version: 2.4.9
+Version: 2.5.0
 Author: WPFactory
 Author URI: https://wpfactory.com
 Text Domain: cost-of-goods-for-woocommerce
 Domain Path: /langs
-Copyright: © 2021 WPFactory
-WC tested up to: 5.7
+Copyright: © 2022 WPFactory
+WC tested up to: 6.1
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -63,7 +63,7 @@ final class Alg_WC_Cost_of_Goods {
 	 * @var   string
 	 * @since 1.0.0
 	 */
-	public $version = '2.4.9';
+	public $version = '2.5.0';
 
 	/**
 	 * @var   Alg_WC_Cost_of_Goods The single instance of the class
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpcodefactory, kerbhavik, karzin
 Tags: woocommerce, cost, cost of goods, cog, cost of goods sold, cogs, woo commerce
 Requires at least: 4.4
 Tested up to: 5.8
-Stable tag: 2.4.9
+Stable tag: 2.5.0
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -97,6 +97,9 @@ And then you can follow these steps:
 6. Cost of goods columns in "Analytics > Orders" report.
 
 == Changelog ==
+
+= 2.5.0 - 19/01/2022 =
+* WC tested up to: 6.1.
 
 = 2.4.9 - 06/10/2021 =
 * Fix - Add cost of goods on orders placed by WooCommerce REST API.
