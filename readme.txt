45f915d6d5661cea2f13e2026afb5cd8796ece80
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2024-10-14 11:38:32 -0300

    v3.5.4

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpcodefactory, omardabbas, karzin, anbinder, algoritmika, kousikmu
 Tags: woocommerce, cost, cost of goods, profit, profit calculator
 Requires at least: 6.1
 Tested up to: 6.6
-Stable tag: 3.5.3
+Stable tag: 3.5.4
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -343,6 +343,10 @@ Once activated, access the plugin's settings by navigating to “WooCommerce > S
 6. Cost of goods columns in "Analytics > Orders" report.
 
 == Changelog ==
+
+= 3.5.4 - 14/10/2024 =
+* Dev - WPFactory admin menu removed from pro version.
+* Fix - WPFactory menu and submenus are now visible and accessible.
 
 = 3.5.3 - 08/10/2024 =
 * Dev - Tabs removed from WPFactory submenu plugin pages.
--- vendor/wpfactory/wpfactory-cross-selling/src/php/class-wpfactory-cross-selling.php
+++ vendor/wpfactory/wpfactory-cross-selling/src/php/class-wpfactory-cross-selling.php
@@ -2,7 +2,7 @@
 /**
  * WPFactory Cross-Selling
  *
- * @version 1.0.0
+ * @version 1.0.1
  * @since   1.0.0
  * @author  WPFactory
  */
@@ -32,7 +32,7 @@ if ( ! class_exists( 'WPFactory\WPFactory_Cross_Selling\WPFactory_Cross_Selling'
 		 *
 		 * @var string
 		 */
-		protected $version = '1.0.0';
+		protected $version = '1.0.1';
 
 		/**
 		 * Setup args.
@@ -91,7 +91,7 @@ if ( ! class_exists( 'WPFactory\WPFactory_Cross_Selling\WPFactory_Cross_Selling'
 		/**
 		 * Setups the class.
 		 *
-		 * @version 1.0.0
+		 * @version 1.0.1
 		 * @since   1.0.0
 		 *
 		 * @param $args
@@ -117,7 +117,7 @@ if ( ! class_exists( 'WPFactory\WPFactory_Cross_Selling\WPFactory_Cross_Selling'
 			$args['admin_page'] = wp_parse_args( $args['admin_page'], array(
 				'page_title' => __( 'WPFactory Recommendations', 'wpfactory-cross-selling' ),
 				'menu_title' => __( 'Recommendations', 'wpfactory-cross-selling' ),
-				'capability' => 'manage_options',
+				'capability' => class_exists( 'WooCommerce' ) ? 'manage_woocommerce' : 'manage_options',
 				'position'   => 2
 			) );
 
