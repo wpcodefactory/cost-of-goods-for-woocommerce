0578d71aa18c34b6a837bb23dc2e902668541df8
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2026-03-12 16:10:44 -0300

    v4.1.0

--- includes/class-alg-wc-cog.php
+++ includes/class-alg-wc-cog.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'Alg_WC_Cost_of_Goods' ) ) :
 		 * @since 1.0.0
 		 * @var   string
 		 */
-		public $version = '4.0.9';
+		public $version = '4.1.0';
 
 		/**
 		 * @since 1.0.0
